9a8c5141b59e0eb221f7a99bafd96a5d4e3f9c2e
Alan <alanlim25@gmail.com>
2015-10-10 00:04:45 +0000

    Only user allowed to change lessons

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,17 +1,28 @@
 class Instructor::LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :remove_authorized_for_current_session
 
 	def new
-		@section = Section.find(params[:section_id])
 		@lesson = Lesson.new
 	end
 
 	def create
-		@section = Section.find(params[:section_id])
-		@lesson = @section.lessons.create(lesson_params)
-		redirect_to instructor_course_path(@section.course)
+		@lesson = current_session.lessons.create(lesson_params)
+		redirect_to instructor_course_path(current_session.course)
 	end
 
 	private 
+
+	def remove_authorized_for_current_session
+		if current_session.course.user != current_user
+			return render :text 'Unauthorized', :status => :unauthorized
+		end
+	end
+
+	helper_method :current_session
+	def current_session
+		@current_session ||= Section.find(params[:section_id])
+	end
 
 	def lesson_params
 		params.require(:lesson).permit(:title, :subtitle)
